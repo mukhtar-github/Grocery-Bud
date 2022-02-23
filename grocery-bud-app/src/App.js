848ba7960cf1bb2759cceeb6f125616426232622
Mukhtar <mukhtartanimugarba@gmail.com>
2022-02-24 00:15:24 +0100

    Update file

--- grocery-bud-app/src/App.js
+++ grocery-bud-app/src/App.js
@@ -1,6 +1,16 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from 'react';
+import List from './List';
+import Alert from './Alert';
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem('lsit');
+  
+  if (list) {
+    return (list = JSON.parse(localStorage.getItem('lsit')));
+  } else {
+    return [];
+  }
+};
 
 function App() {
   return <h2>grocery bud setup</h2>
