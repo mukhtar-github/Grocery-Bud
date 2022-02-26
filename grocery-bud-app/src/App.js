98eae22d8dc409b677b5d387412f8c97ec6bbe73
Mukhtar <mukhtartanimugarba@gmail.com>
2022-02-26 10:39:21 +0100

    Update file

--- grocery-bud-app/src/App.js
+++ grocery-bud-app/src/App.js
@@ -69,11 +69,36 @@ function App() {
 
   useEffect(() => {
     localStorage.setItem('list', JSON.stringify(list));
-  })
+  }, [list]);
 
-
-
-  return <h2>grocery bud setup</h2>
+  return (
+    <section className='section-center'>
+      <form className='grocery-form' onSubmit={handleSubmit}>
+        {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+        <h3>grocery bud</h3>
+        <div className='form-control'>
+          <input
+            type='text'
+            className='grocery'
+            placeholder='e.g eggs'
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <button type='submit' className='submit-btn'>
+            {isEditing ? 'edit' : 'submit'}
+          </button>
+        </div>
+      </form>
+      {list.length > 0 && (
+        <div className='grocery-container'>
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button className='clear-btn' onClick={clearList}>
+            clear items
+          </button>
+        </div>
+      )}
+    </section>
+  );
 }
 
 export default App
